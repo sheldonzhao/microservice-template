c1a0ed4f0f3fa2baf38e73e2d75b2a93719677b9
SheldonZhao <messixaviinsta0303@163.com>
2019-11-21 16:13:00 +0800

    add eureka server

--- eureka-server/src/main/java/com/sheldon/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/sheldon/eurekaserver/EurekaServerApplication.java
@@ -2,8 +2,15 @@ package com.sheldon.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Sheldon Zhao
+ * @date 2019-11-18
+ */
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
---
+++ eureka-server/src/main/java/com/sheldon/eurekaserver/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.sheldon.eurekaserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Sheldon Zhao
+ * @date 2019-11-18
+ */
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable(); //close csrf
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
